0252549b5f0808b9e848a13585664ccccd8f9d39
Grant Prudlow <grant@prudlow.com>
2015-06-08 16:29:56 -0500

    Adjusted layout of card with sidebar open

--- freshscript.user.js
+++ freshscript.user.js
@@ -1,38 +1,53 @@
 // ==UserScript==
 // @name         FreshScript (Jira Tweaks)
 // @namespace    https://youtu.be/hBe0VCso0qs
-// @version      0.4
+// @version      0.5
 // @description  Now, this is a story all about how my life got flipped-turned upside down...
 // @author       The Fresh Prince of Jira
 // @match        https://*.atlassian.net/secure/RapidBoard.jspa*
 // @grant        none
 // @require      http://hammerjs.github.io/dist/hammer.min.js
 // ==/UserScript==
+
 jQuery(function($) {
     
     // Styles
+
     var style = document.createElement("style");
-    style.innerText = ".ghx-extra-fields, .ghx-type, .ghx-days { display: none; }" + 
-        ".ghx-band-2 .ghx-issue .ghx-issue-content { padding: 5px 5px 5px 13px; }" + 
-        ".ghx-band-2 .ghx-issue .ghx-flags { top: 7px; right: 7px; left: initial; }" + 
-        ".ghx-band-2 .ghx-issue .ghx-avatar:not(:empty) ~ .ghx-flags { top: 32px; }" + 
-        ".ghx-band-1 .ghx-issue .ghx-avatar {top: 7px;}" + 
-        ".ghx-priority[title='Major'] { display: none; }" + 
-        "#ghx-pool.wide { padding-top: 0 !important; }" + 
-        "#ghx-pool.wide .ghx-columns { width: 150%; }" + 
-        "#ghx-pool.wide .ghx-column-header-group { position: static !important; }" + 
-        "#ghx-column-headers .ghx-column { white-space: nowrap; text-overflow: ellipsis; overflow: hidden; }" + 
+
+    style.innerText =
+        ".ghx-extra-fields, .ghx-type, .ghx-days { display: none; }" +
+        "#ghx-pool .ghx-issue .ghx-issue-content { padding: 5px 5px 5px 13px; }" +
+        "#ghx-pool.ghx-band-1 .ghx-issue .ghx-issue-fields { padding-right: 25px; }" +
+        "#ghx-pool .ghx-issue .ghx-flags { top: 7px; right: 7px; left: auto; }" +
+        "#ghx-pool.ghx-band-1 .ghx-issue .ghx-flags { top: 5px; right: 4px; }" +
+        "#ghx-pool .ghx-issue .ghx-avatar { top: 5px; right: 5px; left: auto; }" +
+        "#ghx-pool.ghx-band-1 .ghx-issue .ghx-priority," +
+        "#ghx-pool.ghx-band-1 .ghx-issue .ghx-priority img { width: 14px; height: 14px; }" +
+        "#ghx-pool .ghx-issue .ghx-avatar:not(:empty) ~ .ghx-flags { top: 32px; }" +
+        "#ghx-pool.ghx-band-1 .ghx-issue .ghx-avatar:not(:empty) ~ .ghx-flags { top: 29px; }" +
+        "#ghx-pool .ghx-issue .ghx-priority[title='Major'] { display: none; }" +
+        "#ghx-pool.wide { padding-top: 0 !important; }" +
+        "#ghx-pool.wide .ghx-columns { width: 150%; }" +
+        "#ghx-pool.wide .ghx-column-header-group { position: static !important; }" +
+        "#ghx-column-headers .ghx-column { white-space: nowrap; text-overflow: ellipsis; overflow: hidden; }" +
         "#ghx-column-headers .ghx-column h2 { display: inline; }";
+    
     document.head.appendChild(style);
 
     // Column width toggle
+
     $("<button></button>")
         .addClass("aui-button")
-        .text("Toggle Column Width").css("margin-left", 10)
+        .text("Toggle Column Width")
+        .css("margin-left", 10)
         .click(function() {
             $("#ghx-pool").toggleClass("wide");
             $("#ghx-column-header-group").width($(".ghx-columns").width());
-        }).prependTo("#ghx-modes-tools");
+        })
+        .prependTo("#ghx-modes-tools");
+
+    // Click & drag scroll
 
     $.getScript("https://hammerjs.github.io/dist/hammer.min.js", function() {
         // Hammer time
@@ -61,4 +76,5 @@ jQuery(function($) {
             }
         });
     });
+
 });
