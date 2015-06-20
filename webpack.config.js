448a2cfb9715dee165ab6fa988c41d946ca46ed9
Grant Prudlow <grant@prudlow.com>
2015-06-19 19:02:18 -0500

    Fixed version not incrementing on watch builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
 var webpack = require('webpack');
+var ConcatSource = require('webpack/lib/ConcatSource');
 var fs = require('fs');
 
 var versionedBannerPlugin = {
 	apply: function(compiler) {
-		// Bump version (borrowed from bump-webpack-plugin)
-		var json = require('./package.json');
-		var versions = json.version.split('.');
-		versions[2] = parseInt(versions[2]) + 1;
-		json.version = versions.join('.');
-		fs.writeFile('./package.json', JSON.stringify(json, null, 2));
-		// Build banner
-		var template = fs.readFileSync('./src/banner.txt', 'utf8');
-		var banner = template.replace('{{version}}', json.version);
-		// Pass to default banner plugin
-		var bannerPlugin = new webpack.BannerPlugin(banner, { raw: true });
-		bannerPlugin.apply(compiler);
+		compiler.plugin('compilation', function(compilation) {
+			// Bump version (borrowed from bump-webpack-plugin)
+			var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+			var versions = json.version.split('.');
+			versions[2] = parseInt(versions[2]) + 1;
+			json.version = versions.join('.');
+			fs.writeFile('./package.json', JSON.stringify(json, null, 2));
+			// Build banner
+			var template = fs.readFileSync('./src/banner.txt', 'utf8');
+			var banner = template.replace('{{version}}', json.version);
+			// Prepend to output (borrowed from webpack.BannerPlugin)
+			compilation.plugin('optimize-chunk-assets', function(chunks, callback) {
+				chunks.forEach(function(chunk) {
+					chunk.files.forEach(function(file) {
+						compilation.assets[file] = new ConcatSource(banner, '\n', compilation.assets[file]);
+					});
+				});
+				callback();
+			});
+		});
 	}
 };
 
