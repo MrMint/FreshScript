f078edebfab6c80d5d20787cc801bda842868ebc
Grant Prudlow <grant@prudlow.com>
2015-11-18 13:36:09 -0600

    Moved custom menu items in Board dropdown to new "Fresh" button

--- src/board-dropdown.js
+++ src/board-dropdown.js
@@ -1,18 +1,56 @@
 import $ from 'jquery';
 
-let $section = null;
+let $menu = null;
 
 export function addMenuItem(text) {
-	// Create section if it doesn't exist yet
-	if ($section == null) {
-		let $list = $('#ghx-view-pluggable .aui-list');
-		$list.find('ul.aui-last').removeClass('aui-last');
-		$section = $('<ul></ul>')
-			.addClass('aui-list-section aui-last')
-			.appendTo($list);
+
+	// Create dropdown if it doesn't exist yet
+	if ($menu == null) {
+
+		// Create container for dropdown in header
+		let $container = $('<div></div>')
+			.css({ display: 'inline-block', verticalAlign: 'top', paddingLeft: 10 })
+			.prependTo('#ghx-modes-tools');
+
+		// Create menu section
+		$menu = $('<ul></ul>')
+			.addClass('aui-list-section aui-first aui-last');
+
+		// Add dropdown menu to container
+		let $dropdown = $('<div></div>').addClass('ajs-layer box-shadow active').append(
+			$('<div></div>').addClass('aui-list').append($menu)
+		).appendTo($container).hide();
+
+		// Add button to container
+		$('<button>Fresh</button>')
+			.addClass('aui-button ghx-dropdown-trigger')
+			.click(function() {
+				$dropdown.css({
+					width: 200,
+					position: 'absolute',
+					left: $(this).position().left + $(this).outerWidth() - 200,
+					top: $(this).position().top + $(this).outerHeight()
+				}).toggle();
+				$('body').click(); // Close other dropdowns
+				return false;
+			})
+			.appendTo($container);
+
+		// Add active class to menu items on hover
+		$dropdown.on('mouseenter mouseout', 'li', function(e) {
+			$(this).toggleClass('active', e.type == 'mouseenter');
+		});
+
+		// Close dropdown on blur
+		$('body').on('click', '*', function() {
+			$dropdown.hide();
+		});
+
 	}
+
 	// Add menu item
-	let $item = $('<li></li>').addClass('aui-list-item').appendTo($section);
+	let $item = $('<li></li>').addClass('aui-list-item').appendTo($menu);
 	return $('<a></a>').addClass('aui-list-item-link')
 		.attr('href', '#').text(text).appendTo($item);
+
 }
